9365c407a2148a3bcba2d1e2a565119199b54cc3
Juan Penalver <juanpenalversanchez11@gmail.com>
2023-03-23 11:54:43 +0000

    suitcard

--- src/app.js
+++ src/app.js
@@ -6,6 +6,53 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const iconSuit = ["♦", "♥", "♠", "♣"];
+  const numberSuit = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomCard = array => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    let randomElement = array[randomIndex];
+    return randomElement;
+  };
+  randomCard(iconSuit);
+  randomCard(numberSuit);
+
+  // let number = document.querySelector("#number");
+  // let iconTop = document.querySelector("#icon-top");
+  // let iconBottom = document.querySelector("#icon-bottom");
+
+  // number.innerHTML = randomCard(numberSuit);
+  // iconBottom.innerHTML = iconTop.innerHTML = randomCard(iconSuit);
+
+  let button = document.querySelector("#btn");
+  button.addEventListener("click", () => {
+    let cards = document.querySelectorAll("#card");
+
+    cards.forEach(card => {
+      card.querySelector("#number").innerHTML = randomCard(numberSuit);
+      card.querySelector("#icon-top").innerHTML = card.querySelector(
+        "#icon-bottom"
+      ).innerHTML = randomCard(iconSuit);
+    });
+  });
 };
+
+let button = document.querySelector("#btn");
+button.addEventListener("click", () => {
+  let card = document.querySelector("#card");
+  let cloneCard = card.cloneNode(true);
+  document.querySelector(".cardHolder").appendChild(cloneCard);
+});
